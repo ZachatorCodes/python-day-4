9a66a9847aadfda4e6fd0aec08ea1f6a133e6b15
Zach Schwartz <104332807+ZachatorCodes@users.noreply.github.com>
2024-03-21 20:49:28 +0000

    Finish interactive coding exercise

---
+++ heads_or_tails_coding_exercise.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+
+rand_num = random.randint(0, 1)
+if rand_num == 0:
+    print("Tails")
+else:
+    print("Heads")
